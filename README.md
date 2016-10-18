7dada688199c1ffe241eeca8074bd8022cae91f9
Toni Spets <toni.spets@iki.fi>
2016-10-18 15:48:41 +0300

    Builds on MonoDevelop, does not run on Mono (PInvoke)

---
+++ README.md
@@ -0,0 +1,4 @@
+UltimaMono
+==========
+
+This could potentially be a port of UltimaXNA to MonoGame.
---
+++ UltimaMono/dev/Ultima/Audio/AudioService.cs
@@ -0,0 +1,35 @@
+﻿/***************************************************************************
+ *   AudioService.cs
+ *   Copyright (c) 2015 UltimaXNA Development Team
+ * 
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+#region usings
+using UltimaXNA.Core.Audio;
+#endregion
+
+namespace UltimaXNA.Ultima.Audio
+{
+    public class AudioService
+    {
+        public void Update()
+        {
+        }
+
+        public void PlaySound(int soundIndex, AudioEffects effect = AudioEffects.None, float volume = 1.0f)
+        {
+        }
+
+        public void PlayMusic(int id)
+        {
+        }
+
+        public void StopMusic()
+        {
+        }
+    }
+}
